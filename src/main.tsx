05417f54de13a50252c20d257c20fdf72118ca50
Abhishek <abhibhardwaj1017@gmail.com>
2024-08-29 21:57:08 +0530

    Revert "Feat: RTK query setup"
    This reverts commit 7cf3b93e402ce19b5aa25fd0e41d0a745c974626.

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "@mantine/core/styles.css";
-import { store } from "@/services/store";
-import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </StrictMode>
 );
