6ba054fa2df07668190871869958ddd6e2b2b05c
Abhishek <abhibhardwaj1017@gmail.com>
2024-09-14 00:02:29 +0530

    Feat: Lazy pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { Suspense, ComponentType, lazy } from "react";
 import "@mantine/core/styles.css";
 import "@mantine/charts/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { Routes, Route } from "react-router-dom";
-import { HomePage } from "./pages/Home.page";
 import { Header } from "./components";
 import { CurrencyProvider } from "@/context/Currency-Context.tsx";
+
+const LazyHomePage = lazy(() => import("./pages/Home.page"));
+
+interface LazyPageProps {
+  Component: ComponentType;
+}
+
+function LazyPage({ Component }: LazyPageProps) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Component />
+    </Suspense>
+  );
+}
 
 export default function App() {
   return (
@@ -13,7 +27,7 @@ export default function App() {
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<LazyPage Component={LazyHomePage} />} />
           </Routes>
         </main>
       </MantineProvider>
